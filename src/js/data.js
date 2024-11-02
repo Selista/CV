5940c8b88dfdd2ee81c63fd2b2ca6f95812064f1
Tanya Tsykalenko <nejsonya@gmail.com>
2024-11-02 11:12:29 +0200

    Add gitignore file

---
+++ src/js/data.js
@@ -0,0 +1,29 @@
+const data = {
+    "first_name": "Tanya",
+    "last_name": "Tsykalenko",
+    "job_title": "Junior Front-end developer",
+    "photo": "images/portrait.jpg",
+    "phone": "+38-050-723-722-9",
+    "email": "nejsonya@gmail.com",
+    "linkedin": "linkedin.com",
+    "address": "Kyiv, Ukraine",
+    "certificates": "images/certificate_js.jpg",
+    "certificates_html": "images/certificate_html.jpg",
+    "skills": [
+        "Build a simple HTML text site",
+        "Style web pages using CSS",
+        "Program websites with JavaScript",
+        "HTML/CSS/Bootstrap",
+        "JavaScript",
+        "HTML semantics",
+        "CSS and JavaScript animation",
+        "React",
+    ],
+    "education": [
+        ["1994-2000", "Oles Honchar Dnipro National University"],
+        ["2023", "ITGid education platform HTML for JS developers course"],
+        ["2023", "ITGid education platform JavaScript v2.0 course"],
+        ["2024", "ITGid education platform ReactJS course"],
+    ],
+    "profile": "I am a beginner in programming in JavaScript, HTML, CSS, in which I have a great desire and inspiration to become a professional (for this I am ready to work diligently and learn quickly). Professional in the field of media law. Knowledge of foreign languages: Ukrainian - native, Russian - native, English - B2(in progress). I can work both in a team and independently. Sociable, responsible, goal-oriented."
+};
---
+++ src/js/experience.js
@@ -0,0 +1,26 @@
+const experience = [
+    {
+        "company": "private entrepreneur",
+        "city": "Kyiv",
+        "from": "2004",
+        "to": 'Present',
+        "position": "The private entrepreneur",
+        "description": "Management of the Ukrainian designers online store. Assistance in concluding international contracts."
+    },
+    {
+        "company": "The Tonis Television channel ",
+        "city": "Kyiv",
+        "from": "2008",
+        "to": '2011',
+        "position": "The lawyer",
+        "description": "Conducting contract work (development of standard contracts, approval of draft contracts). Participation in claims work related to non-fulfillment (improper fulfillment) of contracts. Participation in the preparation of documents for filing with the court. Legal support for investigative journalism. Representing the interests of the company in court, economic court."
+    },
+    {
+        "company": "The Vecherniye Vesti newspaper",
+        "city": "Kyiv",
+        "from": "2002",
+        "to": '2007',
+        "position": "The lawyer at the editorial office",
+        "description": "Pre-print legal examination of newspaper materials, legal support for journalistic investigations, representation of the interests of the editorial board in court, economic court."
+    }
+]
