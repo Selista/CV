2b2ea9ef17ff1a3cad61391e8b9f447d2799b14a
Tanya Tsykalenko <nejsonya@gmail.com>
2024-11-19 18:12:43 +0200

    Add react certificate.

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,10 @@ document.querySelector('.profile-data').textContent = data.profile;
 /**
  * section data
  */
+
 let expirienceData = '';
 experience.forEach(item => {
-    expirienceData += `<section class="mb-5">`;
+    expirienceData += `<section class="mb-4">`;
     expirienceData += ` <h4 class="company-name mb-2">${item.company} - ${item.city}, <i>${item.from} - ${item.to}</i></h4>`;
     expirienceData += `<h5>${item.position}</h5>`;
     expirienceData += `<p>${item.description}</p>`;
@@ -20,9 +21,11 @@ document.querySelector('.experience-list').innerHTML = expirienceData;
 
 document.querySelector('.contacts-block').innerHTML += `<li><a href="tel:${data.phone}">${data.phone}</a></li>`
 document.querySelector('.contacts-block').innerHTML += `<li><a href="mailto:${data.email}">${data.email}</a></li>`
-document.querySelector('.contacts-block').innerHTML += `<li><a href="${data.linkedin}">${data.linkedin}</a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><a href="${data.linkedin}">Linkedin</a></li>`
+document.querySelector('.contacts-block').innerHTML += `<li><a href="${data.github}">Github</a></li>`
 document.querySelector('.address').innerHTML = `<p>${data.address}</p>`;
 document.querySelector('#my-photo').src = data.photo;
+document.querySelector('.projects-github').innerHTML = `<a href="${data.github}">Github</a>`;
 
 /**
  * skills
@@ -33,6 +36,22 @@ data.skills.forEach(item => {
     skillsData += `<li>${item}</li>`;
 });
 document.querySelector('.skills-list').innerHTML = skillsData;
+
+/**
+ * stages
+ */
+
+let stagesData = '';
+data.stages.forEach(item => {
+    stagesData += `<li>${item}</li>`;
+});
+document.querySelector('.project-list').innerHTML = stagesData;
+
+let stagesDataMessage = '';
+data.stages_message.forEach(item => {
+    stagesDataMessage += `<li>${item}</li>`;
+});
+document.querySelector('.project-message-list').innerHTML = stagesDataMessage;
 
 /**
  * education
@@ -47,6 +66,6 @@ document.querySelector('.education-list').innerHTML = educationData;
 /**
  * certificates
  */
-
-document.querySelector('#my-сertificate').src = data.certificates;
 document.querySelector('#my-сertificate-html').src = data.certificates_html;
+document.querySelector('#my-сertificate').src = data.certificates;
+document.querySelector('#my-сertificate-react').src = data.certificates_react;
